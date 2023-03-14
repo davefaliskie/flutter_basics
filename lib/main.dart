dcc8f56933b48a18014404c5a151e05aa7e073d2
Dave Faliskie <davefaliskie@gmail.com>
2023-03-13 22:43:59 -0300

    Configure custom material color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: '1ManStartup',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: primary,
       ),
       home: const HomeScreen(),
     );
