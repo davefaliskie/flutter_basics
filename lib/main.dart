0ed2aeeaa5e99906d1536777b9b440a30735e2c7
Dave Faliskie <davefaliskie@gmail.com>
2023-03-15 22:35:57 -0300

    Align Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/colors.dart';
 import 'package:flutter_basics/user.dart';
-import 'package:social_login_buttons/social_login_buttons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,50 +35,13 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("1ManStartup"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Login Below",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            SocialLoginButton(
-              onPressed: () => debugPrint("Handle Google Login"),
-              buttonType: SocialLoginButtonType.google,
-              borderRadius: 55,
-            ),
-            SocialLoginButton(
-              onPressed: () => debugPrint("Handle Apple Login"),
-              buttonType: SocialLoginButtonType.appleBlack,
-              borderRadius: 55,
-            ),
-            SocialLoginButton(
-              onPressed: () => debugPrint("Handle Twitter Login"),
-              buttonType: SocialLoginButtonType.twitter,
-              borderRadius: 55,
-            ),
-            SocialLoginButton(
-              onPressed: () => debugPrint("Handle Facebook Login"),
-              buttonType: SocialLoginButtonType.facebook,
-              borderRadius: 55,
-            ),
-            SocialLoginButton(
-              onPressed: () => debugPrint("Handle GitHub Login"),
-              buttonType: SocialLoginButtonType.github,
-              borderRadius: 55,
-            ),
-            SocialLoginButton(
-              onPressed: () => debugPrint("Handle Microsoft Login"),
-              buttonType: SocialLoginButtonType.microsoft,
-              borderRadius: 55,
-            )
-          ],
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("Sign In"),
+          ),
         ),
       ),
     );
