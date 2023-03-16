5981d561c1b756d2b987769bccd2e5897fc0fcc1
Dave Faliskie <davefaliskie@gmail.com>
2023-03-15 22:26:13 -0300

    Launcher Icons Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/colors.dart';
-import 'package:flutter_basics/date_time_format.dart';
-import 'package:flutter_basics/duration_format.dart';
-import 'package:flutter_basics/string_extension.dart';
 import 'package:flutter_basics/user.dart';
 
 void main() {
@@ -39,33 +36,10 @@ class HomeScreen extends StatelessWidget {
         title: const Text("1ManStartup"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              user.name.capitalize(),
-              style:
-                  const TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
-            ),
-            Text(
-              "Logged in at: ${user.login.fullDate()}",
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300),
-            ),
-            Text(
-              "For ${user.duration.secToTime()} time",
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                debugPrint("Person as Json: ${user.toJson()}");
-              },
-              child: const Text("Convert to Json"),
-            ),
-          ],
-        ),
-      ),
+          child: Text(
+        "Subscribe!",
+        style: Theme.of(context).textTheme.titleLarge,
+      )),
     );
   }
 }
