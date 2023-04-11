9e6913755ba4819b30e7b11daf4e360952c8423f
Dave Faliskie <davefaliskie@gmail.com>
2023-04-11 13:10:13 -0400

    Confirm dialog before Navigation

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -5,9 +5,37 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool unsavedChanges = true;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profile"),
+        leading: BackButton(onPressed: () {
+          if (unsavedChanges == true) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text("Are you sure?"),
+                  actions: [
+                    TextButton(
+                      child: const Text("No"),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                    TextButton(
+                      child: const Text("Yes"),
+                      onPressed: () => Navigator.of(context).popUntil(
+                        ModalRoute.withName('/'),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          } else {
+            Navigator.of(context).pop();
+          }
+        }),
       ),
       body: Container(
         color: Colors.blue[300],
