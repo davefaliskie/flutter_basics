634213542d6c8511f8c7a4530d08f604cc75795a
Dave Faliskie <davefaliskie@gmail.com>
2023-04-11 13:12:27 -0400

    WillPopScope example

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -7,38 +7,43 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     bool unsavedChanges = true;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profile"),
-        leading: BackButton(onPressed: () {
-          if (unsavedChanges == true) {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: const Text("Are you sure?"),
-                  actions: [
-                    TextButton(
-                      child: const Text("No"),
-                      onPressed: () => Navigator.of(context).pop(),
-                    ),
-                    TextButton(
-                      child: const Text("Yes"),
-                      onPressed: () => Navigator.of(context).popUntil(
-                        ModalRoute.withName('/'),
-                      ),
-                    ),
-                  ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Profile"),
+          leading: BackButton(
+            onPressed: () {
+              if (unsavedChanges == true) {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Are you sure?"),
+                      actions: [
+                        TextButton(
+                          child: const Text("No"),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                        TextButton(
+                          child: const Text("Yes"),
+                          onPressed: () => Navigator.of(context).popUntil(
+                            ModalRoute.withName('/'),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 );
-              },
-            );
-          } else {
-            Navigator.of(context).pop();
-          }
-        }),
-      ),
-      body: Container(
-        color: Colors.blue[300],
+              } else {
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ),
+        body: Container(
+          color: Colors.blue[300],
+        ),
       ),
     );
   }
