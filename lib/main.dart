55e4a2f8eb29fd65291dc1cbbbe2963cc22521a6
Dave Faliskie <davefaliskie@gmail.com>
2023-05-01 14:18:25 -0400

    Reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/colors.dart';
-import 'package:flutter_basics/data.dart';
-import 'package:flutter_basics/duration_format.dart';
 import 'package:flutter_basics/profile_screen.dart';
 import 'package:flutter_basics/user.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
-  if (kReleaseMode == true) {
-    await SentryFlutter.init(
-      (options) {
-        options.dsn =
-            'https://a24f0c5d63ec482fa7438ef5c88d8b1f@o1137058.ingest.sentry.io/4504996481138688';
-        options.tracesSampleRate = 0.01;
-      },
-      appRunner: () => runApp(const MyApp()),
-    );
-  } else {
-    runApp(const MyApp());
-  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -66,27 +51,11 @@ class HomeScreen extends StatelessWidget {
         width: double.infinity,
         height: double.infinity,
         color: Colors.amber,
-        child: ListView.builder(
-          itemCount: usersList.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text(usersList[index].name),
-                subtitle: Text(
-                  "Logged in for ${usersList[index].duration.secToTime()}",
-                ),
-                leading: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.person, size: 30),
-                  ],
-                ),
-                trailing: const Icon(
-                  Icons.star,
-                ),
-              ),
-            );
-          },
+        child: Center(
+          child: Text(
+            "Subscribe",
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
         ),
       ),
     );
